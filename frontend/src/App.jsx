42b34e8baa275ba05c18031edaa7cf43ab67a7b2
Suyash Chavan <suyashchavanofficial@gmail.com>
2025-08-13 23:19:19 +0530

    Installed Shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="bg-red-500">Click me</Button>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
 
-export default App
+export default App;

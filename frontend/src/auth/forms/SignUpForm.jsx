efc3fe0c3a5acb61a74e29685191270239ddc8a4
Suyash Chavan <suyashchavanofficial@gmail.com>
2025-08-15 01:46:56 +0530

    Created Sign Up page UI

--- frontend/src/auth/forms/SignUpForm.jsx
+++ frontend/src/auth/forms/SignUpForm.jsx
@@ -1,9 +1,133 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username should be atleast of 3 characters" })
+    .max(50),
+  email: z.string().email({ message: "Invalid Email Address" }),
+  password: z
+    .string()
+    .min(5, { message: "Password should be atleast of 5 characters" }),
+});
 
 const SignUpForm = () => {
-  return (
-    <div>SignUpForm</div>
-  )
-}
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-export default SignUpForm
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl sm:max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* Left */}
+        <div className="flex-1">
+          <Link
+            to={"/"}
+            className="font-bold text-2xl sm:text-4xl flex flex-wrap"
+          >
+            <span className="text-red-600">SKNCOE</span>
+            <span className="text-blue-900">NSS</span>
+          </Link>
+          <h2 className="text-[24px] md:text-[30px] font-bold leading-[140%]tracking-tighter pt-5 sm:pt-12">
+            Create a new Account
+          </h2>
+          <p className="text-slate-500 text-[14px] font-medium leading-[140%] md:text-[16px] md:font-normal mt-2">
+            Welcome to the Official SKNCOE NSS Website. Please provide your
+            details
+          </p>
+        </div>
+
+        {/* Right */}
+        <div className="flex-1">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+
+                    <FormControl>
+                      <Input type="text" placeholder="Username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email Address</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="xyz@email.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+
+                    <FormControl>
+                      <Input type="password" placeholder="* * * *" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="bg-blue-500 w-full">
+                Submit
+              </Button>
+            </form>
+          </Form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Already have an account?</span>
+            <Link to={"/sign-in"} className="text-blue-500"> Sign In</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;

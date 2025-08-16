458eaf56d9982f4bdf25235742078eaf2112e093
Suyash Chavan <suyashchavanofficial@gmail.com>
2025-08-16 18:19:19 +0530

    Created Dashboard and Dashboard Sidebar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import BottomNavbar from "@/components/shared/BottomNavbar";
+import DashboardProfile from "@/components/shared/DashboardProfile";
+import DashboardSidebar from "@/components/shared/DashboardSidebar";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
 
-export default Dashboard
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row w-full">
+      {/* Sidebar */}
+      <div className="hidden md:block">
+        <DashboardSidebar />
+      </div>
+
+      <BottomNavbar />
+
+      {/* Dashboard Profile */}
+      <div>{tab === "profile" && <DashboardProfile />}</div>
+    </div>
+  );
+};
+
+export default Dashboard;

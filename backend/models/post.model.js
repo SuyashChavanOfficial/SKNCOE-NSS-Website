5cbfe4469d24d78474c7d94a772c2fb5a8c17f23
Suyash Chavan <suyashchavanofficial@gmail.com>
2025-08-28 18:29:17 +0530

    Created Post Model

---
+++ backend/models/post.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    image: {
+      type: String,
+      required: true,
+      default:
+        "https://images.pexels.com/photos/518543/pexels-photo-518543.jpeg",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;

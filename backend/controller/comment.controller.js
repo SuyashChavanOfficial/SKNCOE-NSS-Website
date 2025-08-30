07ab06e01ebf8c60a43b43a233b13b560db9401d
Suyash Chavan <suyashchavanofficial@gmail.com>
2025-08-30 14:07:47 +0530

    Created api route to edit comment

--- backend/controller/comment.controller.js
+++ backend/controller/comment.controller.js
@@ -40,7 +40,7 @@ export const likeComment = async (req, res, next) => {
     const comment = await Comment.findById(req.params.commentId);
 
     if (!comment) {
-      next(errorHandler(404, "Comment not found!"));
+      return next(errorHandler(404, "Comment not found!"));
     }
 
     const userIndex = comment.likes.findIndex(
@@ -58,6 +58,38 @@ export const likeComment = async (req, res, next) => {
     await comment.save();
 
     res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found!"));
+    }
+
+    if (comment.userId.toString() !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not authorised to edit this comment!")
+      );
+    }
+
+    if (!req.body.content || !req.body.content.trim()) {
+      return next(errorHandler(400, "Comment cannot be empty."));
+    }
+
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(editedComment);
   } catch (error) {
     next(error);
   }

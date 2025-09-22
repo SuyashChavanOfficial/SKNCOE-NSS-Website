04fe035baa44edc9ccc65e8eb5d30aa9f6878ecb
Suyash Chavan <suyashchavanofficial@gmail.com>
2025-09-22 16:40:02 +0530

    Updated Footer

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -33,8 +33,8 @@ const Footer = () => {
               </Link>
             </li>
             <li>
-              <Link to={"/news"} className="hover:text-white">
-                Contact
+              <Link to={"/activities"} className="hover:text-white">
+                Activites
               </Link>
             </li>
           </ul>
@@ -44,11 +44,18 @@ const Footer = () => {
         <div>
           <h2 className="text-lg font-semibold mb-4">Contact Us</h2>
           <p className="text-gray-400 text-sm">
-            NSS Room, -2 Floor, Old SKNCOE Building
+            <span className="font-bold">Address: </span>NSS Room, -1 Floor, Old
+            SKNCOE Building
           </p>
           <p className="text-gray-400 text-sm">Vadgaon Bk., Pune - 41</p>
           <p className="text-gray-400 text-sm">
-            <span className="font-bold">Email: </span>skncoenss@gmail.com
+            <span className="font-bold">Email: </span>
+            <a
+              href="mailto:skncoenss@gmail.com"
+              className="hover:text-white"
+            >
+              skncoenss@gmail.com
+            </a>
           </p>
         </div>
       </div>
@@ -57,14 +64,32 @@ const Footer = () => {
       <div className="mt-8 border-t border-gray-700 pt-6 text-center text-gray-500 text-sm">
         <p>Follow us on:</p>
         <div className="flex justify-center space-x-4 mt-3">
-            <a href="#" className="hover:text-white">Facebook</a>
-            <a href="https://instagram.com/skncoe_nss?igshid=MzRlODBiNWFlZA==" className="hover:text-white">Instagram</a>
-            <a href="https://youtube.com/@NSSSKNCOE" className="hover:text-white">Youtube</a>
-            <a href="https://www.linkedin.com/in/nss-skncoe" className="hover:text-white">LinkedIn</a>
-            <a href="https://twitter.com/skncoe_nss" className="hover:text-white">X</a>
+          <a href="#" className="hover:text-white">
+            Facebook
+          </a>
+          <a
+            href="https://instagram.com/skncoe_nss?igshid=MzRlODBiNWFlZA=="
+            className="hover:text-white"
+          >
+            Instagram
+          </a>
+          <a href="https://youtube.com/@NSSSKNCOE" className="hover:text-white">
+            Youtube
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nss-skncoe"
+            className="hover:text-white"
+          >
+            LinkedIn
+          </a>
+          <a href="https://twitter.com/skncoe_nss" className="hover:text-white">
+            X
+          </a>
         </div>
 
-        <p className="mt-4">&copy; {new Date().getFullYear()} SKNCOE NSS. All Rights Reserved.</p>
+        <p className="mt-4">
+          &copy; {new Date().getFullYear()} SKNCOE NSS. All Rights Reserved.
+        </p>
       </div>
     </div>
   );
